6bb3d2c0ea32f98e4b34bf242e268001927caad2
Suzzet Vazquez Jimenez <71349832+su443@users.noreply.github.com>
2020-10-22 17:15:10 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/svj/a7s212021prac5/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.isc.svj.a7s212021prac5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
